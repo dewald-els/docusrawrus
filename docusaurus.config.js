1756f0879d66d189fc31696405b2c4b12d148fab
Dewald Els <dewald.els@arribatec.com>
2022-11-09 19:18:04 +0100

    feat: Add Auto Deploy to github pages

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,17 +8,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://dewald-els.github.io",
+  baseUrl: "/docusrawrus/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
-
+  organizationName: "dewald-els", // Usually your GitHub org/user name.
+  projectName: "docusrawrus", // Usually your repo name.
+  deploymentBranch: "gh-pages",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
